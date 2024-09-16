48904c88e50884f982ba151e7b7220177f62dba0
Rounak kumar <rounakkumar190504@gmail.com>
2024-09-16 19:01:48 +0530

    some changes

--- app.js
+++ app.js
@@ -133,8 +133,7 @@ app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
     let {id, reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
-    let result = await Listing.findById(id);
-    console.log(result);
+    
     res.redirect(`/listings/${id}`);
 }))
 
